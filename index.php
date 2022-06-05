6d2e877694114614568d7cbee523885f9be3fd05
Nicholas Kruschke <dhaupt88@gmail.com>
2022-06-05 17:18:57 +0200

    Nicks Awesome Project

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-hallo
+
+ <body>
+
+<?php
+include 'header.html';
+include 'nav.html';
+?>
+
+
+  <div class="col-6 col-s-9">
+    <h1>Day One</h1>
+    <p>
+      This is a log of my programming enDEVours.
+    </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat odio nunc, vitae fermentum justo molestie eu. Donec sit amet ante id quam dapibus faucibus sed sed augue. Nam mauris velit, mollis vitae tellus ac, consectetur laoreet dolor. Mauris ultrices bibendum risus, quis molestie nisi. Aliquam eros nulla, efficitur sed augue nec, consectetur dapibus augue. Nam nec porta ante. Nunc ac venenatis nisi. Praesent pharetra ligula eu ligula rhoncus viverra. Praesent eu lorem mollis urna lobortis vulputate in vitae neque. Praesent egestas malesuada neque. Morbi sapien lectus, mattis at nunc in, condimentum gravida est.</p>
+    <p>Etiam egestas elit quis turpis aliquam viverra at quis lectus. Suspendisse semper lorem sed felis tempus interdum. Fusce quam ligula, tincidunt non dui sit amet, porttitor vehicula eros. Donec ac ante rhoncus, pretium magna vitae, dignissim dolor. In vulputate viverra felis at mollis. Phasellus pretium dui hendrerit felis fringilla vulputate. Pellentesque quis malesuada neque. Curabitur scelerisque neque in sem volutpat ornare. Donec libero dui, cursus eget neque vel, vehicula volutpat lacus. Vestibulum odio lacus, dignissim a augue id, hendrerit elementum odio.</p>
+    <p>Nulla volutpat pretium aliquam. Nunc ultricies tellus eget scelerisque imperdiet. Nunc posuere risus sed ligula molestie, eget venenatis nisl posuere. Cras lorem sapien, volutpat vel ipsum volutpat, venenatis volutpat lorem. Nam vehicula gravida nibh vitae laoreet. Sed congue massa nulla. Morbi laoreet magna ac lacus finibus malesuada. Donec vitae finibus leo, id eleifend lectus. Praesent hendrerit consequat congue. Curabitur rhoncus sagittis nibh id dictum. Ut sit amet tellus odio. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+    <p>Aenean in risus arcu. Curabitur nec ante a orci aliquet aliquet. Sed consequat lacinia justo nec dapibus. Nunc ultricies orci sit amet lacus finibus aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet finibus massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque laoreet, elit quis pulvinar luctus, enim neque egestas risus, eu ultricies eros tortor imperdiet ex. Proin in elit nec ex facilisis feugiat. Nullam eget est facilisis, rhoncus quam sed, viverra lectus. Donec vel sapien dui.</p>
+  </div>
+
+  <div class="col-3 col-s-12 content">
+    <div class="aside">
+      <div class="profile">
+        <img src="nick-profile.png">
+      </div>
+        <div>
+        <h2>Who?</h2>
+        <p>I'm Nick, an American living in Germany for the past 7 years.</p>
+        <h2>What?</h2>
+        <p>I'm a Customer Success Engineer with a passion for software and I am trying to catch my break in the industry.</p>
+        <h2>How?</h2>
+        <p>Hopefully the materials on this site display my ability to quickly learn and my motivation to succeed.</p>
+      </div>
+    </div>
+  </div>
+
+<?php
+include 'footer.html';
+?>
+
+</body>
+</html>
